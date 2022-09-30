b09f75115e83dea17c0c8b2790993e379b96d0a9
Ahmad Jumadi <ahmadjumadi61@guru.smk.belajar.id>
2022-09-30 23:55:56 +0700

    React Router 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import * as React from "react";
+import { Routes, Route, Link } from 'react-router-dom';
+import './style.css'; 
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Salam dari Router React</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+// Sebetulnya tidak disarankan tapi gpp lah cuma buat belajar ini, INGETTT..
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Hallo ini halaman homepage</h2>
+        <p>Mari belajar Routing</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Hallo ini halaman About</h2>
+        <p>Mari belajar Routing</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  )
+}
